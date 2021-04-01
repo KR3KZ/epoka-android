836572d6ae307409ae9201ac3e48d91dd0a866d5
KR3KZ <ikreekzi@gmail.com>
2021-04-01 11:55:24 +0200

    login method

--- app/src/main/java/com/example/epoka_android/MainActivity.java
+++ app/src/main/java/com/example/epoka_android/MainActivity.java
@@ -3,49 +3,74 @@ package com.example.epoka_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    String base_url = "http://10.0.2.2:8080/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.lol);
+    }
 
-        String url = "http://10.0.2.2:8080/api/test";
+    public void onLogin(View view) {
+        String url = this.base_url + "api/login";
+        final TextView tv_error = (TextView) findViewById(R.id.tv_error);
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("id", 1);
+            postData.put("password", 1);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        tv_error.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        tv_error.setText(error.toString());
+                    }
+                });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    public void getMissionById (Integer id) {
+        String url = this.base_url + "api/missions";
+        final TextView textView = (TextView) findViewById(R.id.edt_id);
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        try {
-                            String aJsonString = response.getString("name");
-                            textView.setText(response.toString());
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        textView.setText(response.toString());
                     }
                 }, new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         textView.setText("Error: " + error.toString());
-
                     }
                 });
 
